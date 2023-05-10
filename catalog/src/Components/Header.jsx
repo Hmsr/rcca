5c056a0ceb37dfaad0afad896d6b5df9104bed56
Danyang Liu <noelialiu2326@gmail.com>
2023-05-10 17:52:26 +1000

    contact us element added

--- catalog/src/Components/Header.jsx
+++ catalog/src/Components/Header.jsx
@@ -13,7 +13,13 @@ import {
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
-
+const teamMembers = [
+  { name: 'Dany', description: '103902523', href: '#'},
+  { name: 'Hamish', description: '103902523', href: '#'},
+  { name: 'Ericles', description: '103902523', href: '#' },
+  { name: 'Jordan', description: '103902523', href: '#'},
+  { name: 'Vraj', description: '103902523', href: '#'},
+]
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
@@ -49,9 +55,43 @@ export default function Header() {
           <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
             Archive
           </a>
-          <a href="http://rovercarclubaust.asn.au/" className="text-sm font-semibold leading-6 text-gray-900">
+
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
             Contact us
-          </a>
+              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {teamMembers.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex-auto">
+                        <a href={item.href} className="block font-semibold text-gray-900">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                
+              </Popover.Panel>
+            </Transition>
+          </Popover>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
         

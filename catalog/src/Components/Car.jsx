1721fa465f9eb7643545f4159e8bab3f5c3db3c5
Hmsr <hmsrenton@gmail.com>
2023-05-15 13:51:35 +1000

    car

--- catalog/src/Components/Car.jsx
+++ catalog/src/Components/Car.jsx
@@ -1,48 +1,49 @@
-import { useState } from 'react'
-import { useFrame, Canvas } from '@react-three/fiber'
-import { useLoader } from '@react-three/fiber'
-
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
-import car from '../Components/car.stl'
+import { useState } from "react";
+import { useFrame, Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+import { STLLoader } from "three/examples/jsm/loaders/STLLoader";
+import car from "../Components/car.stl";
 
 function CarCar() {
-  const [position, setPosition] = useState([0, 0, 0])
-  const [rotation, setRotation] = useState([0, 0, 0])
-  const [color, setColor] = useState([1, 0, 1])
-  const geometry = useLoader(STLLoader, car)
+  const texture = useLoader(TextureLoader, "path/to/your/texture.jpg");
+  const [position, setPosition] = useState([0, 0, 0]);
+  const [rotation, setRotation] = useState([0, 0, 0]);
+  const [color, setColor] = useState([0.25, 0.25, 0.25]);
+  const geometry = useLoader(STLLoader, car);
 
   useFrame((state, delta) => {
-    setPosition(position => [
-      Math.sin(state.clock.getElapsedTime()) * 2,
-      Math.cos(state.clock.getElapsedTime() * 2) * 2,
-      Math.sin(state.clock.getElapsedTime() * 2) * 2
-    ])
-    setRotation(rotation => [
-      rotation[0] + delta * 2,
-      rotation[1] + delta * 3,
-      rotation[2] + delta * 4
-    ])
-    setColor(color => [
-      (Math.sin(state.clock.getElapsedTime() / 2) + 1) / 2,
-      (Math.cos(state.clock.getElapsedTime() / 2) + 1) / 2,
-      (Math.sin(state.clock.getElapsedTime() / 2 + Math.PI / 2) + 1) / 2
-    ])
-  })
+    setPosition((position) => [
+      Math.sin(state.clock.getElapsedTime()) * 0.6,
+      Math.cos(state.clock.getElapsedTime() * 2) * 0.6,
+      Math.sin(state.clock.getElapsedTime() * 2) * 0.6,
+    ]);
+    setRotation((rotation) => [
+      rotation[0] + delta * 1,
+      rotation[1] + delta * 1,
+      rotation[2] + delta * 1,
+    ]);
+    // setColor((color) => [
+    //   (Math.sin(state.clock.getElapsedTime() / 2) + 1) / 2,
+    //   (Math.cos(state.clock.getElapsedTime() / 2) + 1) / 2,
+    //   (Math.sin(state.clock.getElapsedTime() / 2 + Math.PI / 2) + 1) / 2,
+    // ]);
+  });
 
   return (
     <mesh scale={[0.02, 0.02, 0.02]} position={position} rotation={rotation}>
       <bufferGeometry attach="geometry" {...geometry} />
-      <meshStandardMaterial color={color} />
+      <meshStandardMaterial color={color} map={texture} />
     </mesh>
-  )
+  );
 }
 
 export default function App() {
   return (
     <Canvas>
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <pointLight position={[1, 1, 1]} />
       <CarCar />
     </Canvas>
-  )
+  );
 }

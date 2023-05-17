53fed619cb1409edecf406599a47ebfe35a83336
Jordan Junior <jordan.junior@live.com>
2023-05-17 19:40:22 +1000

    Cleaning some code

--- catalog/src/Components/FilterBar.jsx
+++ catalog/src/Components/FilterBar.jsx
@@ -108,13 +108,6 @@ export default function FilterBar({ onSearch, onYearRangeSelect }) {
 
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
-            {
-              <h2 id="products-heading" className="sr-only">
-                Products
-              </h2>
-            }
-            {/* Filters */}
-            <h3 className="sr-only">Categories</h3>
             <button
               type="button"
               className="block px-2 py-3"
@@ -124,110 +117,6 @@ export default function FilterBar({ onSearch, onYearRangeSelect }) {
             </button>
             {showUploadPopup && <UploadPopup onClose={handleClosePopup} />}
             {/* {showUploadPopup && <Upload onClose={handleClosePopup} />} */}
-            {filters.map((section) => (
-              <Disclosure
-                as="div"
-                key={section.id}
-                className="border-b border-gray-200 py-6"
-              >
-                {({ open }) => (
-                  <>
-                    <h3 className="-my-3 flow-root">
-                      <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                        <span className="font-medium text-gray-900">
-                          {section.name}
-                        </span>
-                        <span className="ml-6 flex items-center">
-                          {open ? (
-                            <MinusIcon className="h-5 w-5" aria-hidden="true" />
-                          ) : (
-                            <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                          )}
-                        </span>
-                      </Disclosure.Button>
-                    </h3>
-                    <Disclosure.Panel className="pt-6">
-                      <div className="space-y-4">
-                        {section.options.map((option, optionIdx) => (
-                          <div key={option.value} className="flex items-center">
-                            <input
-                              id={`filter-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              defaultValue={option.value}
-                              type="checkbox"
-                              defaultChecked={option.checked}
-                              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                            <label
-                              htmlFor={`filter-${section.id}-${optionIdx}`}
-                              className="ml-3 text-sm text-gray-600"
-                            >
-                              {option.label}
-                            </label>
-                          </div>
-                        ))}
-                      </div>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            ))}
-            <Disclosure
-              as="div"
-              /* key={section.id} */ className="border-b border-gray-200 py-6"
-            >
-              {({ open }) => (
-                <>
-                  <h3 className="-my-3 flow-root">
-                    <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                      <span className="font-medium text-gray-900">Year</span>
-                      <span className="ml-6 flex items-center">
-                        {open ? (
-                          <MinusIcon className="h-5 w-5" aria-hidden="true" />
-                        ) : (
-                          <PlusIcon className="h-5 w-5" aria-hidden="true" />
-                        )}
-                      </span>
-                    </Disclosure.Button>
-                  </h3>
-                  <Disclosure.Panel className="pt-6">
-                    <div className="space-y-4">
-                      <div className="flex items-center">
-                        <div className="flex justify-center space-x-2">
-                          <input
-                            className="border border-gray-400 "
-                            type="number"
-                            value={startYear}
-                            min={1986}
-                            max={2023}
-                            onChange={(e) =>
-                              setStartYear(parseInt(e.target.value))
-                            }
-                          />
-                          <span>--</span>
-                          <input
-                            className="border border-gray-400 "
-                            type="number"
-                            value={endYear}
-                            min={1986}
-                            max={2023}
-                            onChange={(e) =>
-                              setEndYear(parseInt(e.target.value))
-                            }
-                          />
-                          <button
-                            className="bg-blue-500 text-white px-1 py-1 rounded"
-                            onClick={handleYearRangeSelect}
-                          >
-                            Apply
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </Disclosure.Panel>
-                </>
-              )}
-            </Disclosure>
           </section>
         </main>
       </div>
